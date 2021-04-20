efc412883892306704a11298b2d91e88d2d9e86c
NickLeMieur <56846437+NickLeMieur@users.noreply.github.com>
2021-04-20 12:54:49 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaUnitTestExample.Tests
+{
+    public class Car
+    {
+        public string Make;
+        private int _speed = 7;  // Backing store
+
+        public int Speed
+        {
+            get => _speed;
+            set
+            {
+                if ((value > 200))
+                {
+                    _speed = 200;
+                }else if((value < -50))
+                {
+                    _speed = -50;
+                }
+                else
+                {
+                    _speed = value;
+                }
+            }
+        }
+        public enum Condition
+        {
+            EXCELLENT,
+            GOOD,
+            FAIR,
+            BAD
+        }
+
+        public Condition Conditions;
+
+        public Car(string mak, Condition cond)
+        {
+            Make = mak;
+            Speed = 0;
+            Conditions = cond;
+        }
+    }
+}        
+
--- FunctionTest.cs
+++ FunctionTest.cs
@@ -1,44 +1,21 @@
 using System;
 using Xunit;
-using LambdaUnitTestExample;
+using static LambdaUnitTestExample.Tests.Car;
 
 // Note: Your project should be named LambdaUnitTestExample.
 namespace LambdaUnitTestExample.Tests
 {
-    public class Car
-    {
-        public string Make;
-        public int Speed;
-        public int Condition;
-
-        public Car(string mak, int con)
-        {
-            Make = mak;
-            Speed = 0;
-            Condition = con;
-        }
-
-    }
     public class CarTest
     {
-        enum Condition
-        {
-            EXCELLENT,
-            GOOD,
-            FAIR,
-            BAD
-        }
-
         [Fact]
         public void TestConstructor()
         {
             // Hint: Conditions is an enum.
-            
-            Car car = new Car("Chevy", (int) Condition.EXCELLENT);
+            Car car = new Car("Chevy", Condition.EXCELLENT);
 
             Assert.Equal("Chevy", car.Make);
             Assert.Equal(0, car.Speed);
-            Assert.Equal((int) Condition.EXCELLENT, car.Condition);
+            Assert.Equal(Condition.EXCELLENT, car.Conditions);
         }
 
         [Theory]
@@ -46,7 +23,7 @@ namespace LambdaUnitTestExample.Tests
         [InlineData(Condition.GOOD)]
         [InlineData(Condition.FAIR)]
         [InlineData(Condition.BAD)]
-        void TestConditions(Condition c)
+        public void TestConditions(Condition c)
         {
             Assert.True(Convert.ToInt32(c) >= 0);
         }
@@ -55,20 +32,20 @@ namespace LambdaUnitTestExample.Tests
         public void TestSpeed()
         {
             // Hint: Speed is a C# Property.
-            Car car = new Car("Ford", (int) Condition.BAD);
+            Car car = new Car("Ford", Condition.BAD);
             Assert.Equal(0, car.Speed);
             car.Speed = 25;
             Assert.Equal(25, car.Speed);
             car.Speed = 200;
             Assert.Equal(200, car.Speed);
             car.Speed = 201;
-            Assert.Equal(201, car.Speed);
+            Assert.Equal(200, car.Speed);
             car.Speed = -25;
             Assert.Equal(-25, car.Speed);
             car.Speed = -50;
             Assert.Equal(-50, car.Speed);
             car.Speed = -51;
-            Assert.Equal(-51, car.Speed);
+            Assert.Equal(-50, car.Speed);
         }
     }
 }
